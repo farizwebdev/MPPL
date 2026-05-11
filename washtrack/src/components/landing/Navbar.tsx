bfcf718fecec3befe72d3ee6da8932126ce18004
farizwebdev <farizabar80@gmail.com>
2026-05-11 17:08:30 +0700

    Ubah Border di Navbar

--- washtrack/src/components/landing/Navbar.tsx
+++ washtrack/src/components/landing/Navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
     <header
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${
         scrolled
-          ? "border-b border-gray-100/80 bg-white/80 shadow-xs backdrop-blur-xl"
+          ? "bg-white/80 shadow-xs backdrop-blur-xl"
           : "bg-transparent"
       }`}
     >
